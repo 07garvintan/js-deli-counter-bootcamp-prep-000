ce6c514fe3bad1b5bfbe046c328e193171b433f9
garvin tan <garvintan07@gmail.com>
2019-06-20 18:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing (array) {
 
 
 function currentLine(line) {
-  var currentLineArray
+  var currentLineArray = []
   
   if (line.length === 0) {
     return "The line is currently empty."
